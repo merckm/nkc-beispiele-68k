4d685544f13d1dee3f4d63498b3b5f83a3d572ef
Martin Merck <martin.merck@gmx.de>
2023-11-21 16:50:00 +0100

    Fixed some GOSI programs and added kcalc

---
+++ CPM-68k/C/Other/kcalc/compat.c
@@ -0,0 +1,186 @@
+/*===========================================================================
+
+  kcalc-cpm
+
+  compat.c
+
+  Some functions missing or broken in the Aztec CP/M C library
+
+  Copyright (c)2021 Kevin Boone, GPLv3.0
+
+===========================================================================*/
+#include "ctype.h"
+#include "compat.h"
+#include "stdio.h"
+
+#ifndef CPM
+/* Aztec C does not have these includes. There's no point replicated
+ * the missing function prototypes, since the compiler doesn't support
+ * function prototypes anyway. */
+#include "string.h"
+#include "stdlib.h"
+#endif
+
+/*
+  memcpy
+*/
+void *_memcpy (dest, src, n)
+void *dest;
+CONST void *src;
+int n;
+  {
+  register int i;
+  for (i = 0; i < n; i++)
+    ((char *)(dest))[i] = ((char *)(src))[i];
+  return dest;
+  }
+
+/*
+  memset
+*/
+void *_memset (s, c, n)
+void *s;
+int c;
+int n;
+  {
+  register int i;
+  for (i = 0; i < n; i++)
+    ((char *)(s))[i] = c;
+  return s;
+  }
+
+/*
+  memmove
+*/
+void *_memmove (dest, src, n)
+void *dest;
+CONST void *src;
+int n;
+  {
+  char *d = (char*)dest;
+  char *s = (char*)src;
+  if (s < d) 
+    {
+    s += n;
+    d += n;
+    while (n--)
+      *--d = *--s;
+    } 
+  else
+    {
+    while (n--)
+      *d++ = *s++;
+    }
+
+  return dest;
+  }
+
+
+/*
+  strdup
+*/
+char *_strdup (s)
+CONST char *s;
+  {
+  int l = strlen (s);
+  char *ret = malloc (l + 1);
+  strcpy (ret, s);
+  return ret; 
+  }
+
+/*
+  strchr
+*/
+char *_strchr (s, c)
+CONST char *s;
+int c;
+  {
+  for (; *s != '\0' && *s != c; ++s)
+    ;
+  return *s == c ? (char *) s : 0;
+  }
+
+
+/*
+  _atof
+  Can't use "atof" for the name, as a broken version already exists
+  in the C library.
+  I hope that using sscant as a substitute for atof() doesn't lead to
+  an infinite recursion -- so far it seems OK. It wouldn't be hard to
+  implement atof(), but if the funcionality already exists in the 
+  C library, it would be wasteful to replicate it.
+*/
+double _atof (str)
+CONST char *str;
+  {
+  double f = 99;
+  sscanf (str, "%lf", &f);
+  return f;
+  }
+
+
+/*
+  _stdtod
+  Can't use "strtod" for the name, as a broken version already exists
+  in the C library.
+*/
+double _strtod (str, ptr)
+char *str;
+char **ptr;
+  {
+  char *p;
+  if (ptr == (char **)0)
+    return _atof (str);
+  
+  p = str;
+  
+  while (isspace (*p))
+    ++p;
+
+  if (*p == '+' || *p == '-')
+    ++p;
+  /* digits, with 0 or 1 periods in it.  */
+  if (isdigit (*p) || *p == '.')
+    {
+      int got_dot = 0;
+      while (isdigit (*p) || (!got_dot && *p == '.'))
+        {
+          if (*p == '.')
+            got_dot = 1;
+          ++p;
+        }
+      /* Exponent.  */
+      if (*p == 'e' || *p == 'E')
+        {
+          int i;
+          i = 1;
+          if (p[i] == '+' || p[i] == '-')
+            ++i;
+          if (isdigit (p[i]))
+            {
+             while (isdigit (p[i]))
+                ++i;
+             *ptr = p + i;
+             return _atof (str);
+            }
+        }
+      *ptr = p;
+      return _atof (str);
+    }
+  /* Didn't find any digits.  Doesn't look like a number.  */
+  *ptr = str;
+  return 0.0;
+  }
+
+/*
+ strupr()
+*/
+void _strupr (s)
+char *s;
+  {
+  while (*s != 0)
+    {
+    *s = toupper (*s);
+    s++;
+    }
+  }
---
+++ CPM-68k/C/Other/kcalc/config.h
@@ -0,0 +1,26 @@
+/*===========================================================================
+
+  config.h
+
+  Various configuration parameters. 
+
+  Kevin Boone, May 2021, GPL v3.0
+
+===========================================================================*/
+#ifndef __CONFIG_H
+#define __CONFIG_H_
+
+/* Character sent by terminal for an interrupt (usually ctrl+c) */
+#define I_INTR  3
+
+/* Character sent by the terminal for the "destuctive backspace" key 
+ * This will usually be chr 8 on a real terminal, but may be 127 when
+ * running the CP/M version under an emulator.
+ */
+#define I_DESTBS 8
+
+/* Character to send to the terminal get non-destructive backspace */
+#define O_BS 8
+
+#endif
+
